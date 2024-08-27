6d5aebefbcd457429203b9b46c9c1dc8254535d0
Fawwaz Ijlal Muqsith <reinjin4@gmail.com>
2024-08-28 00:28:32 +0700

    Belajar tentang Dockerfile FROM dan RUN

---
+++ docker-dockerfile.sh
@@ -0,0 +1,9 @@
+
+#from instruction
+#docker build -t <nama_image> <path>
+docker build -t reinjin/from from
+
+#run instruction
+#docker build -t <nama_image> <path> --progress=plain --no-cache
+docker build -t reinjin/run run
+docker build -t reinjin/run run --progress=plain --no-cache
